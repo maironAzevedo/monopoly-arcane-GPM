4436e2cb5d4d443a788ef83833a8f29142f12203
mairon.azevedo <mairon.azevedo@primeup.com.br>
2022-10-28 22:18:15 -0300

    criação do menu inicial

--- demo1/src/main/java/com/example/demo1/HelloApplication.java
+++ demo1/src/main/java/com/example/demo1/HelloApplication.java
@@ -10,11 +10,18 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        FXMLLoader ApplicationMenuLoader = new FXMLLoader(HelloApplication.class.getResource("appMenu.fxml"));
+        Scene ApplicationMenuScene = new Scene(ApplicationMenuLoader.load());
+        stage.setTitle("Monopoly Arcane");
+        stage.setScene(ApplicationMenuScene);
+        stage.show();
+
+        /*
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("tabuleiro.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Monopoly Arcane");
         stage.setScene(scene);
-        stage.show();
+        stage.show(); */
     }
 
     public static void main(String[] args) {
