f6ca85db2bff263bd5ec3c386f0c2fca2397edac
maironAzevedo <maironazevedo@id.uff.br>
2022-11-13 22:30:28 -0300

    Adicionando controller ao menu inicial

---
+++ monopoly/src/main/java/com/projeto/monopoly/controller/AppMenuController.java
@@ -0,0 +1,14 @@
+package com.projeto.monopoly.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class AppMenuController {
+    @FXML
+    protected void StartGame() {
+        System.out.println("Deu certo!");
+    }
+}
