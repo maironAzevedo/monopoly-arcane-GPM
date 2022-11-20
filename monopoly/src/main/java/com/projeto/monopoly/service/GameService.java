74124d32ce22b5a752be8e4a81cbb8e20eac89ee
maironAzevedo <maironazevedo@id.uff.br>
2022-11-20 01:25:33 -0300

    [MN - 28] Implementação das cartas

--- monopoly/src/main/java/com/projeto/monopoly/service/GameService.java
+++ monopoly/src/main/java/com/projeto/monopoly/service/GameService.java
@@ -8,11 +8,14 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class GameService {
-    /** Este método carrega da base de assets a imagem de dado correspondente ao valor
-     * @param diceValue Valor gerado aleatóriamente
-     * @return Imagem do dado
+
+    /**
+     * Método responsável por carregar a imagem correspondente ao valor do dado
+     * @param diceValue Valor do dado gerado aleatóriamente
+     * @return Imagem do valor correspondente
+     * @throws FileNotFoundException Caso o arquivo não seja encontrado
      */
-    protected Image getDiceImage(int diceValue) throws FileNotFoundException {
+    public static Image getDiceImage(int diceValue) throws FileNotFoundException {
         FileInputStream diceImage;
         String baseUrl = "src/main/resources/com/projeto/monopoly/assets/dices/";
         switch (diceValue) {
@@ -45,7 +48,7 @@ public class GameService {
      * @param firstDiceImage imagem do primeiro dado
      * @param secondDiceImage imagem do segundo dado
      */
-    protected void plotDices(Image firstDiceImage, Image secondDiceImage, AnchorPane anchor) {
+    public static void plotDices(Image firstDiceImage, Image secondDiceImage, AnchorPane dicesAnchor) {
         ImageView firstDiceImageView = new ImageView(firstDiceImage);
         firstDiceImageView.setX(60);
         firstDiceImageView.setY(486);
@@ -58,7 +61,55 @@ public class GameService {
         secondDiceImageView.setPreserveRatio(true);
         secondDiceImageView.setFitHeight(72);
 
-        anchor.getChildren().add(firstDiceImageView);
-        anchor.getChildren().add(secondDiceImageView);
+        dicesAnchor.getChildren().add(firstDiceImageView);
+        dicesAnchor.getChildren().add(secondDiceImageView);
+    }
+
+    /**
+     * Método responsável por realizar a busca da Imagem correspondente à coluna e linha clicada
+     * TODO: Consumir dados do JSON disponível em cardsMapping.json
+     * @param columnIndex Coluna clicada
+     * @param rowIndex Linha clicada
+     * @return Imagem da carta correspondente
+     * @throws FileNotFoundException Caso o arquivo não exista
+     */
+    public static Image getCardImage(int columnIndex, int rowIndex) throws FileNotFoundException {
+        /*JSONParser parser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) parser.parse(new FileReader("cardsMapping.json"));
+        String cardPosition = "ccolumn_" + columnIndex + " row_" + rowIndex;
+        JSONObject cardJson = (JSONObject) jsonObject.get(cardPosition);
+        string ImagePath = (String) cardJson.get("image-path");
+        */
+
+        FileInputStream cardImage;
+        String baseUrl = "src/main/resources/com/projeto/monopoly/assets/cards/";
+        if((columnIndex == 2 && rowIndex == 0) ||
+                (columnIndex == 3 && rowIndex == 10) ||
+                (columnIndex == 10 && rowIndex == 6)) {
+            cardImage = new FileInputStream(baseUrl + "sorte.png");
+            return new Image(cardImage);
+        } else if (columnIndex == 9 && rowIndex == 10) {
+            cardImage = new FileInputStream(baseUrl + "beco-perigoso.png");
+            return new Image(cardImage);
+        } else if (columnIndex == 4 && rowIndex == 10) {
+            cardImage = new FileInputStream(baseUrl + "bar-do-vander.png");
+            return new Image(cardImage);
+        }
+        return null;
+    }
+
+    /**
+     * Método responsável por exibir a carta selecionada na interface gráffica
+     * @param cardImage Imagem selecionada
+     * @param cardsAnchor AnchorPane em que a imagem será exibida
+     */
+    public static void plotCard(Image cardImage, AnchorPane cardsAnchor) {
+        ImageView cardImageView = new ImageView(cardImage);
+        cardImageView.setX(31);
+        cardImageView.setY(217);
+        cardImageView.setPreserveRatio(true);
+        cardImageView.setFitHeight(290);
+
+        cardsAnchor.getChildren().add(cardImageView);
     }
 }
