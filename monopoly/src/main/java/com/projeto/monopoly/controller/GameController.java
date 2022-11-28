3dfdfb28570c850e2e78bc265076e20feb135ae8
jardeson777 <jardeson777@gmail.com>
2022-11-27 23:47:59 -0300

    implementando multijogador e regra de até 2 dados repetidos

--- monopoly/src/main/java/com/projeto/monopoly/controller/GameController.java
+++ monopoly/src/main/java/com/projeto/monopoly/controller/GameController.java
@@ -13,6 +13,7 @@ import org.json.simple.parser.ParseException;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class GameController extends BaseController {
@@ -20,6 +21,12 @@ public class GameController extends BaseController {
 
     @FXML
     private ImageView person1;
+    @FXML
+    private ImageView person2;
+    @FXML
+    private ImageView person3;
+    @FXML
+    private ImageView person4;
 
     @FXML
     private AnchorPane cardsAnchor;
@@ -31,9 +38,27 @@ public class GameController extends BaseController {
     * @return Inteiro que representa a soma dos valores dos dados
     * @throws FileNotFoundException Caso a imagem não seja encontrada
     */
+    int playerCurrent = 1;
+    int qtdDiceEqual = 0;
     @FXML
     public void rollDices() throws FileNotFoundException, InterruptedException {
         dicesAnchor.getChildren().clear();
+
+        ImageView[] players = new ImageView[5];
+        players[1] = person1;
+        players[2] = person2;
+        players[3] = person3;
+        players[4] = person4;
+
+        if(qtdDiceEqual == 2) {
+            qtdDiceEqual = 0;
+
+            if(playerCurrent == 4) {
+                playerCurrent = 1;
+            } else {
+                playerCurrent++;
+            }
+        }
 
         int firstDiceValue = GameService.generateDiceNumber();
         Image firstDiceImage = GameService.getDiceImage(firstDiceValue);
@@ -44,7 +69,20 @@ public class GameController extends BaseController {
         GameService.plotDices(firstDiceImage, secondDiceImage, dicesAnchor);
 
         int totalDiceValue = firstDiceValue + secondDiceValue;
-        GameService.moveCard(person1, totalDiceValue);
+
+        GameService.moveCard(players[playerCurrent], totalDiceValue);
+
+        if(firstDiceValue == secondDiceValue){
+            qtdDiceEqual++;
+            return;
+        }
+
+        playerCurrent++;
+        qtdDiceEqual = 0;
+
+        if(playerCurrent == 5) {
+            playerCurrent = 1;
+        }
 
     }
 
